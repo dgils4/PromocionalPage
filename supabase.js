e0a4c66ec0b5bd63400a3ed9bb5073bd4395e427
dgils4 <63314977+dgils4@users.noreply.github.com>
2026-07-06 01:29:12 -0300

    Criar supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+
+
+const { createClient } = supabase;
+
+const db = createClient(
+ 'https://cdechrtprerblmhllqav.supabase.co',
+ 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkZWNocnRwcmVyYmxtaGxscWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwMjc3MjMsImV4cCI6MjA5MTYwMzcyM30.m-RFl-dDWFsjbWqSZw-y0BDqnpHX5hMH8Z361MCT_Rk'
+);
