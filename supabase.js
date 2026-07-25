7e8bc71995aaa013d84705db5dacf2f95e100963
dgils4 <63314977+dgils4@users.noreply.github.com>
2026-07-24 22:32:26 -0300

    Atualizar o supabase.js

--- supabase.js
+++ supabase.js
@@ -6,3 +6,4 @@ const db = createClient(
  'https://cdechrtprerblmhllqav.supabase.co',
  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkZWNocnRwcmVyYmxtaGxscWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwMjc3MjMsImV4cCI6MjA5MTYwMzcyM30.m-RFl-dDWFsjbWqSZw-y0BDqnpHX5hMH8Z361MCT_Rk'
 );
+const empresaId = Number(localStorage.getItem("empresa_id"));
